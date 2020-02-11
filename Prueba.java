13a445a7c19272cb0733d3376486514bc06d0c2a
DiegoFlores18 <36686852+DiegoFlores18@users.noreply.github.com>
2020-02-11 00:36:55 -0600

    Cálculo de un dato más.

--- Prueba.java
+++ Prueba.java
@@ -1,5 +1,9 @@
 public class Prueba {
 	public static void main(String[] args) {
+		int uno = 0;
+		int dos = 0;
+		int tres = 0;
+		
 		System.out.println("Hola mundo... :3");
 		System.out.println("Prueba. Tarea de Administración y Proyectos de Software");
 		System.out.println("Integrantes: ");
@@ -8,18 +12,20 @@ public class Prueba {
 		System.out.println("Reynoso Galvez Ana Laura.");
 		System.out.println("Rojas Castañeda Karen Arleth.");
 		Scanner reader = new Scanner(System.in);
-		int uno = 0;
-		int dos = 0;
-		int tres = 0;
-			
-		System.out.println("Introduce un número:");			
-		uno = reader.nextInt();
-					
-		System.out.println("Introduce otro número:");
+		
+		System.out.print("Introduce un número: ");			
+		uno = reader.nextInt();			
+		System.out.print("Introduce otro número: ");
 		dos = reader.nextInt();
 		System.out.println("Realizando alguna operación.... ");
-		tres=uno+dos;
+		tres = uno + dos;
 		System.out.println("El resultado de la operación es: " + tres);
+		
+		System.out.println("¿Es número par o impar?");
+                
+                if((tres % 2) == 0){
+                    System.out.println(tres+ " es número par");
+                }else System.out.println(tres + " es número impar");
 		
 		System.out.println("Adios, mundo! ... :3");
 
