32f416e987bc2853dfa2566a7a3e88e4e5c72527
UstehKenny <carosiim.sdco@gmail.com>
2020-02-10 21:40:00 -0600

    Añadiendo operación
    Pide al usuario dos números para realizar la operación suma.

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,20 @@ public class Prueba {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo...");
 		System.out.println("Prueba. Tarea de Administración y Proyectos de Software");
+		Scanner reader = new Scanner(System.in);
+		int a = 0;
+		int b = 0;
+		int r = 0;
+			
+		System.out.println("Introduce un número:");			
+		a = reader.nextInt();
+					
+		System.out.println("Introduce otro número:");
+		b = reader.nextInt();
+		System.out.println("Realizando alguna operación.... ");
+		r=a+b;
+		System.out.println("El resultado de la operación es: " + resultado);
+
 	}
+
 }
